4185693b3e4faca035592817a03c9d507b8adffc
Bruna Da Silva Teixeira <brunateixeira.silvabdst@gmail.com>
2022-09-16 18:35:32 -0300

    Add files via upload

---
+++ queries_oficina.sql
@@ -0,0 +1,15 @@
+INSERT INTO Cliente (Nome, Endereco, Contato) VALUES ('Luana', 'Endereco 1', '987131451');
+INSERT INTO Cliente (Nome, Endereco, Contato) VALUES ('Júnior', 'Endereco 2', '977509006');
+INSERT INTO Cliente (Nome, Endereco, Contato) VALUES ('Paulo', 'Endereco 3', '994708022');
+
+INSERT INTO Oficina (Conserto, Revisao);
+INSERT INTO Oficina (Conserto, Revisao); 
+INSERT INTO Oficina (Conserto, Revisao);
+
+INSERT INTO Mecanico (Codigo, Nome, Endereco, Especialidade) VALUES ('013', 'Anderson', 'Endereco 4', 'motor');
+INSERT INTO Mecanico (Codigo, Nome, Endereco, Especialidade) VALUES ('025', 'Lucas', 'Endereco 5', 'freio');
+INSERT INTO Mecanico (Codigo, Nome, Endereco, Especialidade) VALUES ('038', 'Fernando', 'Endereco 6', 'direcao hidraulica');
+
+INSERT INTO Ordem_Servico (Data_Entrega, Numero, Data_Emissao, Valor, Status) VALUES (2022/09/26, '0290', 2022/09/20, 'Finalizado', 800.00);
+INSERT INTO Ordem_Servico (Data_Entrega, Numero, Data_Emissao, Valor, Status) VALUES (2022/10/03, '0310', 2022/09/28, 'Autorizado', 420.00); 
+INSERT INTO Ordem_Servico (Data_Entrega, Numero, Data_Emissao, Valor, Status) VALUES (2022/10/10, '0317', 2022/09/30, 'Autorizado', 2000.00);
